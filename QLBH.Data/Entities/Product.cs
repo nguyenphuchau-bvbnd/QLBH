a752bd6e7460c113b3c310ea76107d86ef2913f5
Nguyen Phuc Hau <nguyenphuchau.bvbnd@gmail.com>
2021-05-30 11:40:36 +0700

    Tao cáu truc

---
+++ QLBH.Data/Entities/Product.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QLBH.Data.Entities
+{
+    public class Product
+    {
+        
+        public int Id { get; set; }
+        public decimal Price { get; set; }
+        public decimal OriginalPrice { get; set; }
+        public int Stock { get; set; }
+        public int ViewCount { get; set; }
+        public DateTime DateCreated { get; set; }
+        public string SeoAlias { get; set; }
+
+    }
+}
