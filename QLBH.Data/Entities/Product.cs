712c160d63d5554f613615024a2ad32bbd1c0ef9
Nguyen Phuc Hau <nguyenphuchau.bvbnd@gmail.com>
2021-06-06 10:46:38 +0700

    Seeding database

--- QLBH.Data/Entities/Product.cs
+++ QLBH.Data/Entities/Product.cs
@@ -1,19 +1,26 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Text;
 
 namespace QLBH.Data.Entities
 {
     public class Product
     {
+        public int Id { set; get; }
+        public decimal Price { set; get; }
+        public decimal OriginalPrice { set; get; }
+        public int Stock { set; get; }
+        public int ViewCount { set; get; }
+        public DateTime DateCreated { set; get; }
         
-        public int Id { get; set; }
-        public decimal Price { get; set; }
-        public decimal OriginalPrice { get; set; }
-        public int Stock { get; set; }
-        public int ViewCount { get; set; }
-        public DateTime DateCreated { get; set; }
-        public string SeoAlias { get; set; }
+        public List<ProductInCategory> ProductInCategories { get; set; }
+
+        public List<OrderDetail> OrderDetails { get; set; }
+
+        public List<Cart> Carts { get; set; }
+
+        public List<ProductTranslation> ProductTranslations { get; set; }
 
     }
 }
---
+++ QLBH.Data/Enums/OrderStatus.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QLBH.Data.Enums
+{
+    public enum OrderStatus
+    {
+        InProgress,
+        Confirmed,
+        Shipping,
+        Success,
+        Canceled
+    }
+}
